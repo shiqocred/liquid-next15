fe292b4dc407f3b342f54b3caf08b704171201aa
shiqocred <21103080090@student.uin-suka.ac.id>
2025-02-18 21:00:18 +0700

    fix get price

--- src/app/(dashboard)/outbond/sale/create/_components/dialog-discount.tsx
+++ src/app/(dashboard)/outbond/sale/create/_components/dialog-discount.tsx
@@ -12,7 +12,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { cn, numericString } from "@/lib/utils";
 import { AlertCircle, Check, Percent } from "lucide-react";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 const DialogDiscount = ({
   open,
