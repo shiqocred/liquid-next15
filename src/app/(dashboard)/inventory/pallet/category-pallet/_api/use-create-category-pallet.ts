f15612ce2a50542ccb3e3f44bf19f83916c699f8
lusipr <lusipr142@gmail.com>
2025-02-25 09:46:09 +0700

    add category pallet management features and API integration

---
+++ src/app/(dashboard)/inventory/pallet/category-pallet/_api/use-create-category-pallet.ts
@@ -0,0 +1,39 @@
+import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+import type { AxiosResponse } from "axios";
+import { baseUrl } from "@/lib/baseUrl";
+import { toast } from "sonner";
+import { getCookie } from "cookies-next/client";
+
+type RequestType = {
+  body: any;
+};
+
+type Error = AxiosError;
+
+export const useCreateCategoryPallet = () => {
+  const accessToken = getCookie("accessToken");
+
+  const mutation = useMutation<AxiosResponse, Error, RequestType>({
+    mutationFn: async ({ body }) => {
+      const res = await axios.post(`${baseUrl}/category_palets`, body, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      return res;
+    },
+    onSuccess: () => {
+      toast.success("Category successfully created");
+    },
+    onError: (err) => {
+      if (err.status === 403) {
+        toast.error(`Error 403: Restricted Access`);
+      } else {
+        toast.error(`ERROR ${err?.status}: Category failed to create`);
+        console.log("ERROR_CREATE_CATEGORY:", err);
+      }
+    },
+  });
+  return mutation;
+};
