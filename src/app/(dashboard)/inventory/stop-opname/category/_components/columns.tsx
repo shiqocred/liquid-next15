f3c0958a7be53bb9c5a08b9a4f6cc18425391cfc
lusipr <lusipr142@gmail.com>
2025-06-03 21:40:24 +0700

    refactor: Improve action column layout and adjust button styles in category columns

--- src/app/(dashboard)/inventory/stop-opname/category/_components/columns.tsx
+++ src/app/(dashboard)/inventory/stop-opname/category/_components/columns.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { MetaPageProps } from "@/lib/utils-client";
 import { ColumnDef } from "@tanstack/react-table";
-import { ReceiptText } from "lucide-react";
+import { ArrowBigRight, ReceiptText } from "lucide-react";
 import Link from "next/link";
 
 export const columnsSOCategory = ({
@@ -28,7 +28,8 @@ export const columnsSOCategory = ({
     accessorKey: "end_date",
     header: "Period End",
     cell: ({ row }) => (row.original.end_date ? row.original.end_date : "-"),
-  },{
+  },
+  {
     accessorKey: "type",
     header: "Status",
     cell: ({ row }) => {
@@ -51,22 +52,31 @@ export const columnsSOCategory = ({
     id: "action",
     header: "Action",
     cell: ({ row }) => (
-      <div className="flex items-center">
+      <div className="flex items-center p-2 gap-2">
         <Link
           href={
-            row.original.type === "process"
-              ? "/inventory/stop-opname/category/create"
-              : `/inventory/stop-opname/category/detail/${row.original.id}`
+            `/inventory/stop-opname/category/detail/${row.original.id}`
           }
         >
           <Button
             size={"icon"}
             variant={"outline"}
-            className="border-sky-400/80 hover:bg-sky-50 hover:border-sky-400"
+            className="items-center border-yellow-400 text-yellow-700 hover:text-yellow-700 hover:bg-yellow-50"
           >
             <ReceiptText />
           </Button>
         </Link>
+        {row.original.type === "process" && (
+          <Link href="/inventory/stop-opname/category/create">
+             <Button
+            size={"icon"}
+            variant={"outline"}
+            className="border-sky-400/80 hover:bg-sky-50 hover:border-sky-400"
+          >
+            <ArrowBigRight />
+          </Button>
+          </Link>
+        )}
       </div>
     ),
   },
