6aedf98eaeda193c562cfc9b4997f11abeab8a5d
lusipr <lusipr142@gmail.com>
2025-06-29 16:34:25 +0700

    `feat: add new features to b2b components
    * added new column to client table in edit page
    * added new dialog for adding bag in edit page
    * added new dialog for editing list of bags in edit page
    * added new

---
+++ src/app/(dashboard)/outbond/b2b/detail/[b2bId]/_component/dialog-detail.tsx
@@ -0,0 +1,221 @@
+"use client";
+
+import { DataTable } from "@/components/data-table";
+import Pagination from "@/components/pagination";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { usePagination } from "@/lib/pagination";
+import { useSearchQuery } from "@/lib/search";
+import { alertError, cn } from "@/lib/utils";
+import { TooltipProviderPage } from "@/providers/tooltip-provider-page";
+import { Loader, RefreshCw, X } from "lucide-react";
+import React, { useEffect, useMemo } from "react";
+import { useGetDetailBagB2B } from "../_api";
+import { AxiosError } from "axios";
+
+const DialogDetail = ({
+  open,
+  onCloseModal,
+  columns,
+  idBagB2B,
+}: 
+{
+  open: boolean;
+  onCloseModal: () => void;
+  columns: any;
+  idBagB2B: any;
+}) => {
+  const { search, searchValue, setSearch } = useSearchQuery("searchProduct");
+  const { metaPage, page, setPage, setPagination } =
+    usePagination("pageProduct");
+
+  const { data, isPending, refetch, isRefetching, error, isError, isSuccess } =
+    useGetDetailBagB2B({
+      id: idBagB2B,
+      p: page,
+      q: searchValue,
+    });
+
+   const dataListDetailBag: any = useMemo(() => {
+      return data?.data?.data?.resource?.[0] ?? {};
+    }, [data]);
+
+  console.log("dataListDetailBag", dataListDetailBag);
+
+  const dataListDetailBagProduct: any[] = useMemo(() => {
+    return data?.data?.data?.resource?.bulky_sales?.data ?? [];
+  }, [data]);
+
+  const isLoading = isPending || isRefetching;
+
+  useEffect(() => {
+    if (data && isSuccess) {
+      setPagination(data?.data?.data?.resource?.bulky_sales ?? {});
+    }
+  }, [data, isSuccess]);
+  
+
+  useEffect(() => {
+    alertError({
+      isError,
+      error: error as AxiosError,
+      data: "Data Products",
+      action: "get data",
+      method: "GET",
+    });
+  }, [isError, error]);
+
+  useEffect(() => {
+  if (open) {
+    refetch();
+  }
+}, [searchValue, page, open]);
+
+  useEffect(() => {
+    if (!open) {
+      setPage(1);
+      setSearch("");
+    }
+  }, [open]);
+
+  return (
+    <Dialog open={open} onOpenChange={onCloseModal}>
+      <DialogContent
+        onClose={false}
+        className="min-w-[75vw]"
+        onOpenAutoFocus={(e) => e.preventDefault()}
+      >
+        <DialogHeader>
+          <DialogTitle className="justify-between flex items-center">
+            Detail Bag B2B
+            <TooltipProviderPage value="close" side="left">
+              <button
+                onClick={() => onCloseModal()}
+                className="w-6 h-6 flex items-center justify-center border border-black hover:bg-gray-100 rounded-full"
+              >
+                <X className="w-4 h-4" />
+              </button>
+            </TooltipProviderPage>
+          </DialogTitle>
+        </DialogHeader>
+        {isLoading ? (
+          <div className="w-full h-[78vh] flex justify-center items-center">
+            <Loader className="size-6 animate-spin" />
+          </div>
+        ) : (
+          <div className="w-full flex flex-col gap-5 mt-5">
+            <div className="flex w-full rounded-md overflow-hidden border border-sky-400/80 p-5 flex-col">
+              <div className="flex w-full border-sky-400/80 rounded-md overflow-hidden border p-4 flex-col">
+                <div className="w-full flex items-center">
+                  <div className="flex items-center w-full">
+                    <div className="flex w-full items-end gap-4">
+                      <div className="flex flex-col">
+                        <p className="text-xs">Barcode Bag</p>
+                        <p className="font-semibold capitalize text-lg">
+                          {dataListDetailBag?.barcode_bag}
+                        </p>
+                      </div>
+                    </div>
+                    <Separator
+                      orientation="vertical"
+                      className="bg-gray-500 h-20"
+                    />
+                    <div className="flex w-full pl-5 items-end gap-4">
+                      <div className="flex flex-col">
+                        <p className="text-xs">Name Bag</p>
+                        <p className="font-semibold capitalize text-lg">
+                          {dataListDetailBag?.name_bag}
+                        </p>
+                      </div>
+                    </div>
+                    <Separator
+                      orientation="vertical"
+                      className="bg-gray-500 h-20"
+                    />
+                    <div className="flex w-full pl-5 items-end gap-4">
+                      <div className="flex flex-col">
+                        <p className="text-xs">Category Bag</p>
+                        <p className="font-semibold capitalize text-lg">
+                          {dataListDetailBag?.category_bag}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <Separator
+                    orientation="vertical"
+                    className="bg-gray-500 h-20"
+                  />
+                </div>
+                <Separator className="bg-gray-500" />
+                <div className="w-full flex items-center">
+                  <div className="flex items-center w-full">
+                    <div className="flex w-full items-end gap-4">
+                      <div className="flex flex-col">
+                        <p className="text-xs">Status Bag</p>
+                        <p className="font-semibold capitalize text-lg">
+                          {dataListDetailBag?.status}
+                        </p>
+                      </div>
+                    </div>
+                    <Separator
+                      orientation="vertical"
+                      className="bg-gray-500 h-20"
+                    />
+                    <div className="flex w-full pl-5 items-end gap-4">
+                      <div className="flex flex-col">
+                        <p className="text-xs">Total Product</p>
+                        <p className="font-semibold capitalize text-lg">
+                          {dataListDetailBag?.total_product}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center gap-3 w-full">
+              <Input
+                className="w-2/5 border-sky-400/80 focus-visible:ring-sky-400"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                placeholder="Search..."
+                autoFocus
+              />
+              <TooltipProviderPage value={"Reload Data"}>
+                <Button
+                  onClick={() => refetch()}
+                  className="items-center w-9 px-0 flex-none h-9 border-sky-400 text-black hover:bg-sky-50"
+                  variant={"outline"}
+                >
+                  <RefreshCw
+                    className={cn("w-4 h-4", isLoading ? "animate-spin" : "")}
+                  />
+                </Button>
+              </TooltipProviderPage>
+            </div>
+            <DataTable
+              maxHeight="h-[45vh]"
+              isSticky
+              isLoading={isLoading}
+              columns={columns}
+              data={dataListDetailBagProduct ?? []}
+            />
+            <Pagination
+              pagination={{ ...metaPage, current: page }}
+              setPagination={setPage}
+            />
+          </div>
+        )}
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default DialogDetail;
---
+++ src/app/(dashboard)/outbond/b2b/edit/[b2bId]/_components/dialogs/dialog-list-bag-edit.tsx
@@ -0,0 +1,63 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+import {  columnEditListBag } from "../columns";
+import { DataTable } from "@/components/data-table";
+import { TooltipProviderPage } from "@/providers/tooltip-provider-page";
+import { X } from "lucide-react";
+
+export const DialogListBagEdit = ({
+  open,
+  onOpenChange,
+  listIdBag,
+  selectedBagId,
+  onSelectBag,
+}: {
+  open: boolean;
+  onOpenChange: () => void;
+  listIdBag: any;
+  selectedBagId: any;
+  onSelectBag: (id: string) => void;
+}) => {
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent
+        onOpenAutoFocus={(e) => e.preventDefault()}
+        onClose={false}
+        className="max-w-6xl"
+      >
+        <DialogHeader>
+          <DialogTitle className="justify-between flex items-center">
+            Select Buyer
+            <TooltipProviderPage value="close" side="left">
+              <button
+                onClick={() => onOpenChange()}
+                className="w-6 h-6 flex items-center justify-center border border-black hover:bg-gray-100 rounded-full"
+              >
+                <X className="w-4 h-4" />
+              </button>
+            </TooltipProviderPage>
+          </DialogTitle>
+          <DialogDescription />
+        </DialogHeader>
+        <div className="flex flex-col w-full gap-4">
+          <DataTable
+            isSticky
+            maxHeight="h-[60vh]"
+            columns={columnEditListBag({
+              onClose: onOpenChange,
+              onSelectBag,
+              selectedBagId,
+            })}
+            data={listIdBag ?? []}
+          />
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
