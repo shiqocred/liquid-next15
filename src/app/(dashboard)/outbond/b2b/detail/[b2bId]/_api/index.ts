60ef5f350e58cd1e044789586d83d531449cf345
lusipr <lusipr142@gmail.com>
2025-07-08 07:27:43 +0700

    feat: implement remove bag functionality in B2B detail view

--- src/app/(dashboard)/outbond/b2b/detail/[b2bId]/_api/index.ts
+++ src/app/(dashboard)/outbond/b2b/detail/[b2bId]/_api/index.ts
@@ -2,3 +2,4 @@ export * from "./use-get-detail-b2b";
 export * from "./use-get-detail-bag-b2b";
 export * from "./use-export-detail-data-b2b";
 export * from "./use-remove-product-b2b";
+export * from "./use-remove-bag-b2b";
