bd9077b63343a344927875054620b1a5b86f80b6
lusipr <lusipr142@gmail.com>
2025-07-15 15:10:46 +0700

    add bulky product filters for categories, warehouses, brands, conditions, and statuses

---
+++ src/app/(dashboard)/inventory/pallet/list/create/_api/use-get-brands-bulky.ts
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { baseUrlApiBulky } from "@/lib/baseUrl";
+import { getCookie } from "cookies-next/client";
+
+export const useGetBrandsBulky = () => {
+  const accessToken = getCookie("accessToken");
+  const query = useQuery({
+    queryKey: ["list-brands-create-palet"],
+    queryFn: async () => {
+      const res = await axios.get(
+        `${baseUrlApiBulky}/products/filter/brands`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      return res;
+    },
+  });
+  return query;
+};
--- src/lib/baseUrl.ts
+++ src/lib/baseUrl.ts
@@ -1,3 +1,5 @@
 export const urlWeb = process.env.NEXT_PUBLIC_URL_WEB!;
+export const urlApiBulky = process.env.NEXT_PUBLIC_URL_API_BULKY!;
 
 export const baseUrl = `${urlWeb}/api`;
+export const baseUrlApiBulky = `${urlApiBulky}/api`;
