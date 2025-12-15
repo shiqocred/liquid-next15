32b4d965349a1078e6f2ffc8554da325de980fdd
lusipr <lusipr142@gmail.com>
2025-12-15 16:52:43 +0700

    refactor: update useGetDetailABN and useToDisplay to use barcode instead of id

--- src/app/(dashboard)/repair-station/abnormal/_api/use-get-detail-abnormal.ts
+++ src/app/(dashboard)/repair-station/abnormal/_api/use-get-detail-abnormal.ts
@@ -3,13 +3,13 @@ import axios from "axios";
 import { baseUrl } from "@/lib/baseUrl";
 import { getCookie } from "cookies-next/client";
 
-export const useGetDetailABN = ({ id }: any) => {
+export const useGetDetailABN = ({ barcode }: any) => {
   const accessToken = getCookie("accessToken");
   const query = useQuery({
-    queryKey: ["detail-abn", id],
+    queryKey: ["detail-abn", barcode],
     queryFn: async () => {
       const res = await axios.get(
-        `${baseUrl}/new_products/${id}`,
+        `${baseUrl}/new_products/barcode/${barcode}`,
         {
           headers: {
             Authorization: `Bearer ${accessToken}`,
@@ -18,7 +18,7 @@ export const useGetDetailABN = ({ id }: any) => {
       );
       return res;
     },
-    enabled: !!id,
+    enabled: !!barcode,
   });
   return query;
 };
